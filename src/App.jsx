03e06eb145bff58dac724fab4565f5a970ed19c5
BekBrace <bekdevelopment@gmail.com>
2022-06-27 12:02:21 +0200

    change1: finished basic filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {companies} from './companies';
 
 function App() {
+  const [find, setFind] = useState("");
+  // console.log(companies.filter((company)=>company.company_name.toLowerCase().includes("a")));
   return (
     <div className="App">
-      Hello My Friends!
+      <input type="text" 
+      placeholder='Find...' 
+      className="iField" 
+      onChange = {(e)=>setFind(e.target.value)}/>
+      <i className="fa fa-search" aria-hidden="true"></i>
+      <ul className='uList' >
+        {/* {companies.map((company) => (
+          <li className='list'>{company.company_name}</li>
+        ))} */}
+        {companies.filter(company => company.company_name.toLowerCase().includes(find)).map(company =>(
+          <li key = {company.id} className="list">{company.company_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/companies.js
@@ -0,0 +1,127 @@
+export const companies = [
+    {
+    "id": 1,
+    "company_name": "sony",
+    "email":"info@sony.com",
+},
+    {
+    "id": 2,
+    "company_name": "toshiba",
+    "email":"info@toshiba.com",
+},
+    {
+    "id": 3,
+    "company_name": "BASF",
+    "email":"info@basf.com",
+},
+    {
+    "id": 4,
+    "company_name": "Lenovo",
+    "email":"info@lenovo.com",
+    },
+    {
+    "id": 5,
+    "company_name": "Samsung",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 6,
+    "company_name": "IBM",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 7,
+    "company_name": "LG",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 8,
+    "company_name": "PEPSICO",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 9,
+    "company_name": "razer",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 10,
+    "company_name": "Nescafe",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 11,
+    "company_name": "P&G",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 12,
+    "company_name": "GlaxoSmith",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 13,
+    "company_name": "Pfizer",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 14,
+    "company_name": "Nintendo",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 15,
+    "company_name": "Acer",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 16,
+    "company_name": "Ikea",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 17,
+    "company_name": "Leroy Merlin",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 18,
+    "company_name": "Castorama",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 19,
+    "company_name": "Autodistribution",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 20,
+    "company_name": "Casio",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 21,
+    "company_name": "Asus",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 22,
+    "company_name": "HP",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 23,
+    "company_name": "KFC",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 24,
+    "company_name": "Burger King",
+    "email":"info@gmail.com",
+    },
+    {
+    "id": 25,
+    "company_name": "Apple",
+    "email":"info@gmail.com",
+    },
+]
