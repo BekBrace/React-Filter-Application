9ecf62d3985c451a31f5a5739f045ca1de4b43b4
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2022-06-27 18:35:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import './App.css';
-import {companies} from './companies';
+import {data} from './data';
 import TABLE from './componenets/TABLE';
 function App() {
   const [find, setFind] = useState("");
   const search = (data) => {
-    return data.filter(item=>item.company_name.toLowerCase().includes(find))
-  }
+    return data.filter(item => 
+      item.author.toLowerCase().includes(find) || 
+      item.language.toLowerCase().includes(find) ||
+      item.title.toLowerCase().includes(find)||
+      item.country.toLowerCase().includes(find))
+  };
   // console.log(companies.filter((company)=>company.company_name.toLowerCase().includes("a")));
   return (
     <div className="App">
       <input type="text" 
-      placeholder='Find...' 
+      placeholder='Find a book ...' 
       className="iField" 
       onChange = {(e)=>setFind(e.target.value)}/>
-      <TABLE data={search(companies)} />
+      <TABLE data={search(data)} />
     </div>
   );
 }
